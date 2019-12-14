28512cc696fe0fa5abd8c30af575feee8200b085
Filip Troníček <filip.tronicek@seznam.cz>
2019-12-14 15:29:32 +0100

    Update time.js

--- api/time.js
+++ api/time.js
@@ -1,4 +1,4 @@
 module.exports = (req, res) => {
   const epoch = Date.now(); // Unix timestamp in milliseconds
-  res.status(200).send((epoch - (epoch % 100))/100);
+  res.status(200).send((epoch - (epoch % 100))/10);
 };
