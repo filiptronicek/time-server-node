27958c912ed9215002efa6bd844f8d2e2066ca7b
Filip Troníček <filip.tronicek@seznam.cz>
2019-12-14 17:56:08 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -6,7 +6,7 @@ function updateTime() {
     http.onload = () => {
         document.getElementById("time").innerHTML = (http.responseText - (http.responseText % 1000)) / 1000;
         let epoch = new Date().valueOf(); // Unix timestamp in milliseconds
-        epoch = (epoch / 1000).toFixed(0);
+        //epoch = (epoch / 1000).toFixed(0);
         document.getElementById("comp_time").innerHTML = epoch;
         console.log("Server time: " + (http.responseText) );
         console.log("Local time: " + epoch )
