22d922de72b2a1e1ccccf7e0868ef963b8f7a2b7
Filip Troníček <filip.tronicek@seznam.cz>
2019-12-14 19:27:31 +0100

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,4 +1,6 @@
 module.exports = (req, res) => {
   const epoch = Date.now(); // Unix timestamp in milliseconds
+  res.header('Access-Control-Allow-Origin','*');
   res.status(200).send(epoch);
+
 };
