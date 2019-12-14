41b80291180d1d67905b854be7aea3b118a0c8ad
Filip Troníček <filip.tronicek@seznam.cz>
2019-12-14 17:52:55 +0100

    Value of

--- js/main.js
+++ js/main.js
@@ -5,7 +5,7 @@ function updateTime() {
     http.send();
     http.onload = () => {
         document.getElementById("time").innerHTML = (http.responseText - (http.responseText % 1000)) / 1000;
-        let epoch = new Date().getTime(); // Unix timestamp in milliseconds
+        let epoch = new Date().valueOf(); // Unix timestamp in milliseconds
         epoch = (epoch / 1000).toFixed(0);
         document.getElementById("comp_time").innerHTML = epoch;
         console.log("Server time: " + (http.responseText) );
